c9cc4127f839d14de58ffd5df141e1979fcc32a8
jamesrowles <jamesianrowles@gmail.com>
2017-09-28 23:16:03 +1300

    Update linetest.m

--- linetest.m
+++ linetest.m
@@ -8,8 +8,8 @@ dy = (y1-y0)/N;
 x = x0:dx:x1;
 y = y0:dy:y1;
 
-[t1, t2] =xy2a(x,y);
-[v1, v2] =AtoV(t1,t2);
+[t1, t2] =xy2a(x,y); %pass x and y to xy2a.m that will turn the x and y to angle t1 and t2 
+[v1, v2] =AtoV(t1,t2); %pass t1 and t1(t1/2 = angle) AtoV.m will turn yt1/2 into voltages(pwm) call v1 and v2 
 s = size(v1);
 time = 1:s(2);
 plot(time,v1,'r',time,v2, 'b')
