36d30dc61f986aced12b09e7041de19baea3f246
jamesrowles <jamesianrowles@gmail.com>
2017-10-02 10:56:16 +1300

    Update xy2a.m

--- xy2a.m
+++ xy2a.m
@@ -1,30 +1,31 @@
+function [t1,t2] = xy2a(tx,ty)
+%Inputs/Veriables
 m1x = 297.0;
 m1y = 472.0;
 m2x = 475.0;
 m2y = 438.0;
 R = 247.73;
-tx = 
-ty = 
-    
+
 %%%%%%%%%%% MOTOR 1 %%%%%%%%%%%
 Delatx = m1x-tx;
 Deltay = ty-m1y;
 	
-l = ((Deltay)^2+(Delatx)^2)^(1/2);
-h = (R^2-(l/2)^2)^(1/2);
+l = ((Deltay).^2+(Delatx).^2).^(1/2);
+h = (R.^2-(l/2).^2).^(1/2);
+
 Angle1 = acos((Delatx)/l);
 Angle2 = acos(l/(2*R));
 Angle21 = ((Angle2)+(pi-Angle1));
 Angle22 = ((pi-Angle1)-(Angle2));
-	
-	
+
 xj21 = tx + R*cos(pi - Angle21);
 yj21 = ty + R*sin(pi - Angle21);
 xj22 = tx + R*cos(pi - Angle22);
 yj22 = ty + R*sin(pi - Angle22);
 		
-M1Angle11 = -atan2(yj21 - m2y, xj21 - m2x);
-M1Angle12 = -atan2(yj22 - m2y, xj22 - m2x);
+M1Angle11 = -atan2(yj21 - m2y, xj21 - m2x); % needs to be in deg
+M1Angle12 = -atan2(yj22 - m2y, xj22 - m2x); % needs to be in deg
+
 M1LinkageAngle11 = ((M1Angle11*180)/pi);
 M1LinkageAngle12 = ((M1Angle12*180)/pi);
 	
@@ -32,8 +33,9 @@ M1LinkageAngle12 = ((M1Angle12*180)/pi);
 Delatx = m2x-tx;
 Deltay = ty-m2y;
 
-l = ((Deltay)^2+(Delatx)^2)^(1/2);
-h = (R^2-(l/2)^2)^(1/2);
+l = ((Deltay).^2+(Delatx).^2).^(1/2);
+h = (R.^2-(l/2).^2).^(1/2);
+
 Angle1 = acos((Delatx)/l);
 Angle2 = acos(l/(2*R));
 Angle21 = ((Angle2)+(pi-Angle1));
@@ -44,11 +46,12 @@ yj21 = ty + R*sin(pi - Angle21);
 xj22 = tx + R*cos(pi - Angle22);
 yj22 = ty + R*sin(pi - Angle22);
 
-M2Angle21 = -atan2(yj21 - m2y, xj21 - m2x);
+M2Angle21 = -atan2(yj21 - m2y, xj21 - m2x); 
 M2Angle22 = -atan2(yj22 - m2y, xj22 - m2x);
-M2LinkageAngle21 = ((M2Angle21*180)/pi);
-M2LinkageAngle22 = ((M2Angle22*180)/pi);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-t1 = M1Angle11;
-t2 = M2Angle21;
 
+M2LinkageAngle21 = ((M2Angle21*180)/pi); % needs to be in deg
+M2LinkageAngle22 = ((M2Angle22*180)/pi); % needs to be in deg
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+t1=M2Angle21;
+t2=M2Angle22;
