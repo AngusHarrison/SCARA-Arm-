c8e6b62459ed9dd1bdbb46ca64b7ebd04977e4c5
AngusHarrison <angusharrison1@gmail.com>
2017-10-02 12:48:59 +1300

    Update AtoV.m

--- AtoV.m
+++ AtoV.m
@@ -3,7 +3,6 @@ function [vv1,vv2] = AtoV(M1LinkageAngle11,M2LinkageAngle21)
 %%%Conversion of angle to PWM%%%
 v1=1400;
 v2=1600;
-
 a11=127; 
 a12=113;
 
@@ -11,4 +10,5 @@ a12=113;
 vv1 = v1 +(M1LinkageAngle11 -a11)*(v2-v1)/(a12-a11);
 a21=65;
 a22=45;
+
 vv2 = v2 +(M2LinkageAngle21 -a21)*(v2 -v1)/(a22-a21);
